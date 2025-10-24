cae5c4c43307a56a1b9ec3aff9c4ea57abf40c9b
yasir321419 <yasir@chameleon-ideas.com>
2025-10-24 23:40:13 +0500

    changes

--- utils/stripeApis.js
+++ utils/stripeApis.js
@@ -45,8 +45,8 @@ const verifyConnectedAccount = async (accountId) => {
   try {
     const accountLink = await stripe.accountLinks.create({
       account: accountId,
-      refresh_url: "http://3.96.154.115:4000/api/v1/reauth",
-      return_url: `http://3.96.154.115:4000/api/v1/success/${accountId}`,
+      refresh_url: "https://api.cutsy.com/api/v1/reauth",
+      return_url: `https://api.cutsy.com/api/v1/success/${accountId}`,
       // refresh_url: "http://localhost:4000/api/v1/reauth",
       // return_url: `http://localhost:4000/api/v1/success/${accountId}`,
       type: "account_onboarding",
